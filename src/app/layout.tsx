ccbb36de5308e98e51a29440ccf09e1c2b78b33c
Waleed Abdullah <waleed.abdullah910@gmail.com>
2025-06-22 05:42:05 +0000

    Make this the website icon (favicon) that we see in the browser tab: htt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { Toaster } from "@/components/ui/toaster";
 export const metadata: Metadata = {
   title: 'Waleed Abdullah',
   description: 'Personal portfolio showcasing projects and skills in electrical engineering.',
+  icons: {
+    icon: 'https://drive.google.com/uc?export=view&id=1ZvDFyk3RdBEw7qC1INEvq0y4UrxogHJZ',
+  },
 };
 
 export default function RootLayout({
