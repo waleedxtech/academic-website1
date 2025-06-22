826cbfd5ccae48798478a5357a4f63e70d0c9bc4
Waleed Abdullah <waleed.abdullah910@gmail.com>
2025-06-22 06:56:09 +0000

    Change Favicon to: https://res.cloudinary.com/dcajabot9/image/upload/v17

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   title: 'Waleed Abdullah',
   description: 'Personal portfolio showcasing projects and skills in electrical engineering.',
   icons: {
-    icon: 'https://drive.google.com/uc?export=view&id=1ZvDFyk3RdBEw7qC1INEvq0y4UrxogHJZ',
+    icon: 'https://res.cloudinary.com/dcajabot9/image/upload/v1750574746/favicon_txl4mt.ico',
   },
 };
 
