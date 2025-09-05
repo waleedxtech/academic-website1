875a81d947c48be86e8c1b52e4e15dc76b584dd4
waleedxtech <waleedabdullah.ee@gmail.com>
2025-09-05 08:53:48 +0500

    feat: Add source code and configuration files

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: '/academic-website1',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
