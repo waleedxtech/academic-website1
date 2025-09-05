b584bbb66ecd7a3fb27c98af5e04495a3de45b22
waleedxtech <waleedabdullah.ee@gmail.com>
2025-09-05 09:28:47 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: '/academic-website1',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
